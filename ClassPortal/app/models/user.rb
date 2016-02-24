0fe186dc0941043419831a4d5d0e6e9c77518b8a
dhaval0603 <dhaval0603@gmail.com>
2016-02-24 16:48:26 -0500

    Messaging Done!

--- ClassPortal/app/models/user.rb
+++ ClassPortal/app/models/user.rb
@@ -6,6 +6,8 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   attr_accessor :roles
 
+   has_many :conversations, :foreign_key => :sender_id
+
   def self.search(query)
     where("name like ? or email like ? ", "%#{query}%", "%#{query}%")
   end
